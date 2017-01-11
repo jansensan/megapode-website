c95da443d1408b68f7252e011bc4ed3e3522a335
Mat Janson Blanchet <mat.jb@jansensan.net>
2017-01-10 21:09:53 -0500

    Deleted unused home page model

--- src/scripts/pages/home/home-page.js
+++ src/scripts/pages/home/home-page.js
@@ -4,9 +4,6 @@
 
   angular
     .module('mp.pages.HomePage', [
-      // models
-      'mp.models.HomePageModel',
-      // components
       'mp.components.Footer',
       'mp.components.Header',
       'mp.components.HomeBanner',
