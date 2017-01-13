d31ad9138a10cc3f87ea316463a4f63695dd8c26
Mat Janson Blanchet <mat.jb@jansensan.net>
2017-01-12 22:08:44 -0500

    Added animated floating plan in home banner (#17)

--- src/scripts/pages/home/banner/home-banner.js
+++ src/scripts/pages/home/banner/home-banner.js
@@ -13,8 +13,56 @@
     return {
       restrict: 'E',
       scope: {},
+      controller: Controller,
       templateUrl: '/pages/home/banner/home-banner-template.html',
     };
+  }
+
+  function Controller() {
+    var plan = $('#hoveringPlan');
+    var shadow = $('#hoveringPlanShadow');
+    var animDuration = 2000;
+    var animEasing = 'easeInOutCubic';
+
+    // auto activation
+    activate();
+
+    // methods definitions
+    function activate() {
+      slideUp();
+    }
+
+    function slideUp() {
+      plan.animate(
+        {top: '100px'},
+        animDuration,
+        animEasing,
+        slideDown
+      );
+      shadow.animate(
+        {
+          opacity: 0.8
+        },
+        animDuration,
+        animEasing
+      );
+    }
+
+    function slideDown() {
+      plan.animate(
+        {top: '120px'},
+        animDuration,
+        animEasing,
+        slideUp
+      );
+      shadow.animate(
+        {
+          opacity: 1
+        },
+        animDuration,
+        animEasing
+      );
+    }
   }
 
 })();
